7d8479e52793f281b1256cf5ce476707b45fb5f2
Sattar <sattar@rebar.shop>
2025-05-18 11:06:09 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import VoiceChat from './VoiceChat';
+import './style.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<VoiceChat />);
