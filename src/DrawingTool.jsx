ef4c58331f790517e07cbdb270f0afcb5bcd3007
Sattar <sattar@rebar.shop>
2025-06-03 10:47:14 -0400

    Update API endpoint to correct path

--- src/DrawingTool.jsx
+++ src/DrawingTool.jsx
@@ -49,7 +49,7 @@ export default function DrawingTool() {
       files.forEach(file => formData.append("files", file));
       formData.append("mode", mode);
 
-      const res = await axios.post('/api/parse-blueprints', formData, {
+      const res = await axios.post('/api/parse-blueprint-estimate', formData, {
         headers: { "Content-Type": "multipart/form-data" }
       });
 
